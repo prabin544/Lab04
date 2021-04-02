fac4dd83d00a3e8fb3c5fc9063095e2e83a5e337
Prabin Singh <pravin544@gmail.com>
2021-04-01 22:08:03 -0500

    displaying selected horn Beast

--- src/App.js
+++ src/App.js
@@ -10,29 +10,24 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      horn: "",
+      renderBeast: animalArray,
       displayModal: false,
       src: 'https://placehold.it/500x100/444',
     }
   }
 
   showFavePic = (animal) => {
-    // console.log("clicked")
-    // console.log(animal)
     this.setState({     
       src: animal,
       displayModal: true
     })
   }
 
-  getHornNum = (horn) => {
-    console.log("clicked")
-    console.log(horn)
+  getHornNum = (newArray) => {
+    console.log(newArray)
     this.setState({     
-      horn: horn
+      renderBeast: newArray
     })
-    // // console.log(animal)
-    
   }
 
   hideModal = () => {
@@ -43,15 +38,25 @@ class App extends React.Component{
     return(
       <>
         <Header />
+
         <Container>
-          <FilterBeast getHornNum={this.getHornNum}/>          
+          <FilterBeast 
+            getHornNum = {this.getHornNum} 
+            animalArray = {animalArray}
+          />          
         </Container>
-        <Main animalArray = {animalArray} showFavePic={this.showFavePic} horn/>
-        <SelectedBeast 
-        displayModal ={this.state.displayModal}
-        hideModal={this.hideModal}
-        img={this.state.src.imgUrl} 
+
+        <Main 
+          animalArray = {this.state.renderBeast}
+          showFavePic={this.showFavePic}
         />
+
+        <SelectedBeast 
+          displayModal ={this.state.displayModal}
+          hideModal={this.hideModal}
+          img={this.state.src.imgUrl} 
+        />
+
       </>
     )
   }
--- src/components/FilterBeast.js
+++ src/components/FilterBeast.js
@@ -4,18 +4,23 @@ import { Form, Button } from 'react-bootstrap';
 class FilterBeast extends React.Component {
     onFormSubmit(event) {
         event.preventDefault();
-        // console.log("Horn num is :", this.state.num);
+        const horns = +this.state.num
         this.props.getHornNum(this.state.num)
+        let newArray = this.props.animalArray.filter(animal => animal.horns === horns)
+        this.props.getHornNum(newArray);
     }
+
     onChangeNum(event) {
         this.setState({ num: event.target.value })
     }
+
     render() {
+      console.log(this.props)
       return (
         <Form onSubmit={this.onFormSubmit.bind(this)} role="form">
           <Form.Group controlId="exampleForm.SelectCustom">
             <Form.Label>Search Beast by Number of Horns : </Form.Label>
-            <Form.Control as="select" custom onChange={this.onChangeNum.bind(this)}>
+            <Form.Control as="select" onChange={this.onChangeNum.bind(this)}>
               <option value="0">Zero</option>
               <option value="1">One</option>
               <option value="2">Two</option>
